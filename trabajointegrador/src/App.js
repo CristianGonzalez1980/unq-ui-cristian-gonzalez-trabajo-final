6e722f95901eadd9311167eb55dbc19795c181f3
CristianGonzalez1980 <GitHub1980>
2020-07-21 13:29:18 -0300

    seAgregoSaludoIncialyDetallesDisenio

--- trabajointegrador/src/App.js
+++ trabajointegrador/src/App.js
@@ -7,40 +7,39 @@ import spock from './images/spock.png';
 import tijera from './images/tijera.png';
 import lagarto from './images/lagarto.png';
 
+
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       userselection: undefined,
       machineselection: undefined,
-      lastResult: "Elige una opción",
+      welcomeMsg: "Bienvenido!",
+      lastResult: "¿Piedra, Papel, Tijera, Lagarto o Spock?",
       round: 0,
       win: 0,
       lose: 0,
+      tie: 0,
       showText: false,
-      showOption: true,
+      showOption: false,
       showButton: true,
       showCpuText: false,
       showAlert: false,
+      showAlertInfo: true,
       showAlertDanger: false,
       showAlertSucces: false,
       showAlertWarning: false,
-      itemSel: "",
       options: ['Piedra', 'Papel', 'Tijera', 'Lagarto', 'Spock'],
     };
   }
 
   newGame = () => {
-    this.setState({ showCpuText: false, showOption: true, showButton: false, showAlertDanger: false, showAlertSucces: false, showAlertWarning: false })
+    this.setState({ userselection: undefined, machineselection: undefined, showCpuText: false, showOption: true, showButton: false, showAlertInfo: true, showAlertDanger: false, showAlertSucces: false, showAlertWarning: false, lastResult: "Elige una opción" })
   }
 
   resetGame = () => {
-    this.setState({ userselection: undefined });
-    this.setState({ machineselection: undefined });
-    this.setState({ round: 0 });
-    this.setState({ win: 0 });
-    this.setState({ lose: 0 });
-    this.setState({ lastResult: "Volvamos a empezar" })
+    this.newGame()
+    this.setState({ round: 0, win: 0, lose: 0, lastResult: "Volvamos a empezar" });
   }
 
   startMachinePlay = () => {
@@ -51,7 +50,7 @@ class App extends React.Component {
   }
 
   showStadistics = () => {
-    return ("Ronda n° " + this.state.round + "    Ganaste: " + this.state.win + "    Perdiste: " + this.state.lose);
+    return ("Ronda " + this.state.round + "    Ganó: " + this.state.win + "    Perdió: " + this.state.lose + "    Empató: " + this.state.tie);
   }
 
   checkResult = () => {
@@ -109,7 +108,7 @@ class App extends React.Component {
   userDoSelection = event => {
     const userselection = event.target.id
     this.setState({ userselection });
-    this.setState({ showOption: false, showButton: false })
+    this.setState({ showText: true, showOption: false, showButton: false, showAlertInfo: false })
     this.startMachinePlay();
   }
 
@@ -117,7 +116,7 @@ class App extends React.Component {
     return (
       <>
         {
-          this.state.showOption && (<img src={img} alt={text} className="cuadro" id={text} onMouseEnter={() => this.setState({ showText: true, itemSel: text })}
+          this.state.showOption && (<img src={img} alt={text} className="cuadro" id={text} onMouseEnter={() => this.setState({ showText: true, userselection: text })}
             onMouseLeave={() => this.setState({ showText: false })} onClick={(event) => this.userDoSelection(event)} />)
         }
       </>
@@ -126,14 +125,14 @@ class App extends React.Component {
 
   mostrarSelecion = (id, fShow, fState, text) => {
     return (
-      <>
+      <div className="bar-sel">
         <div className="row-sel" id={id}>
           <div>{text}</div>
         </div>
         <div className="row-sel" id={id}>
           {fShow && (<div className="selText"><span>{fState}</span></div>)}
         </div>
-      </>
+      </div>
     )
   }
 
@@ -141,6 +140,8 @@ class App extends React.Component {
     return (
       <>
         <div className="row">
+          <div>{this.state.showAlertInfo && (<div className="welcome"><div>{this.state.welcomeMsg}</div><div>{this.state.lastResult}</div></div>)}
+          </div>
           <div>{this.state.showAlertDanger && (<h5 className="alert alert-danger"> {this.state.lastResult} </h5>)}
           </div>
           <div>{this.state.showAlertSucces && (<h5 className="alert alert-success"> {this.state.lastResult} </h5>)}
@@ -161,19 +162,21 @@ class App extends React.Component {
           </header>
           <body className="App-body">
             <div className="container">
-              <div className="row">
-                <div className="col-sel">
-                  {this.mostrarSelecion("hum", this.state.showText, this.state.itemSel, "Tu elección: ")}
+              <div className="row justify-content-around">
+                <div className="col-4.5">
+                  {this.mostrarSelecion("hum", this.state.showText, this.state.userselection, "Tu elección: ")}
                 </div>
-                <div className="col-auto">
-                  {this.mostrarOpcion(tijera, "Tijera")}
-                </div>
-                <div className="col-sel">
+                <div className="col-4.5">
                   {this.mostrarSelecion("cpu", this.state.showCpuText, this.state.machineselection, "Elección robot: ")}
                 </div>
               </div>
-              <div className="row">
+              <div className="row justify-content-around">
                 <div className="col-auto">
+                  {this.mostrarOpcion(tijera, "Tijera")}
+                </div>
+              </div>
+              <div className="row justify-content-around">
+                <div className="col-3-9">
                   {this.mostrarOpcion(spock, "Spock")}
                 </div>
                 <div className="col-btn-auto">
@@ -181,18 +184,18 @@ class App extends React.Component {
                     {this.state.showButton && <button type="button" className="btn-danger" onClick={() => this.newGame()}>Jugar</button>}
                   </div>
                   <div>
-                    {this.state.showAlert && <h3 className="alert alert-warning"> {this.state.lastResult} </h3>}
+                    {this.state.showAlert && <h5 id="msg" className="alert alert-warning"> {this.state.lastResult} </h5>}
                   </div>
                 </div>
-                <div className="col-auto">
+                <div className="col-3-9">
                   {this.mostrarOpcion(papel, "Papel")}
                 </div>
               </div>
-              <div className="row">
-                <div className="col-auto">
+              <div className="row justify-content-around">
+                <div className="col-4">
                   {this.mostrarOpcion(lagarto, "Lagarto")}
                 </div>
-                <div className="col-auto">
+                <div className="col-4">
                   {this.mostrarOpcion(piedra, "Piedra")}
                 </div>
               </div>
@@ -200,12 +203,10 @@ class App extends React.Component {
           </body>
           <div className="App-bottom" id="botom-text">
             <div className="row">
-              <div className="col-auto">
-                <h1 className="text-success"> {this.showStadistics()} </h1>
-              </div>
-              <div className="col-auto">
-                <button type="button" className="btn btn-primary" onClick={() => this.resetGame()}>Reiniciar Marcador</button>
-              </div>
+              <h1 className="text-success"> {this.showStadistics()} </h1>
+            </div>
+            <div className="row">
+              {this.state.showButton && <button type="button" className="btn-reset" onClick={() => this.resetGame()}>Reiniciar Marcador</button>}
             </div>
           </div>
         </div>
